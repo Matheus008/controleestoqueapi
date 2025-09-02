c62fff8c4a77b28bbf502b7b0900ab6d4e268a10
Matheus Wassermann <matheus5wassermann@gmail.com>
2025-09-02 20:58:57 -0300

    Criado ranking de produto mais vendido e usuário que mais vendeu.

---
+++ src/main/java/com/estoque/controle/model/fornecedor/FornecedorDTO.java
@@ -0,0 +1,18 @@
+package com.estoque.controle.model.fornecedor;
+
+public record FornecedorDTO(String nomeFornecedor, String cpfOuCnpj, TipoFornecedor tipoFornecedor) {
+
+    public static FornecedorDTO of(String nomeFornecedor, String cpfOuCnpj, TipoFornecedor tipoFornecedor) {
+        if (cpfOuCnpj != null) {
+            cpfOuCnpj = cpfOuCnpj.replaceAll("\\D", ""); // para ficar somente números
+            if (cpfOuCnpj.length() == 11 && tipoFornecedor == TipoFornecedor.FISICA) {
+                cpfOuCnpj = cpfOuCnpj.replaceAll("(\\d{3})(\\d{3})(\\d{3})(\\d{2})",
+                        "$1.$2.$3-$4");
+            } else if (cpfOuCnpj.length() == 14 && tipoFornecedor == TipoFornecedor.JURIDICA) {
+                cpfOuCnpj = cpfOuCnpj.replaceAll("(\\d{2})(\\d{3})(\\d{3})(\\d{4})(\\d{2})",
+                        "$1.$2.$3/$4-$5");
+            }
+        }
+        return new FornecedorDTO(nomeFornecedor, cpfOuCnpj, tipoFornecedor);
+    }
+}
